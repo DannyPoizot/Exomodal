69e3272a6c7f2a3cda9536c4a183458d608d2095
Danny Poizot <poizot.dan@gmail.com>
2021-05-11 11:49:35 +0200

    exo modal

---
+++ js/modal.js
@@ -0,0 +1,19 @@
+let modal = document.getElementById("myModal");
+
+let btn = document.getElementById("myBtn");
+
+let span =document.getElementsByClassName("close")[0];
+
+btn.onclick = function(){
+  modal.style.display = "block";
+}
+
+span.onclick = function(){
+  modal.style.display = "none";
+}
+
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none";
+  }
+}
